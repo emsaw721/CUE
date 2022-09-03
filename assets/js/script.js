9140e42ef9d73af11c28350f12340562bd23222f
Emily Hill <emhillmed2021@gmail.com>
2022-09-02 22:15:51 -0500

    added outline to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,12 @@ function generateQuestion() {
        // setCounterText();
 
 }; 
+
+
+//1. First page is introduction with click start quiz button . 
+//2. Timer starts and presented with a question.
+//   a. Answer question from list of answers. 
+//3. Presented with another question. 
+//   a. If question is answered incorrectly, time is subracted from the clock. 
+//4. All questions answered or timer reaches 0, game is over. 
+//   a. Can save initials and score. 
