98a0413e7a511c8963084d3cbc2c459d81349736
Emily Hill <emhillmed2021@gmail.com>
2022-09-02 22:07:28 -0500

    html, css, and js

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+
+var generateBtn = document.querySelector("#generate");
+
+var generateQuestion = Function
+
+function quizStart() {
+
+
+
+}; 
+
+generateBtn.addEventListener("click", quizStart())
+   if(click) { clearInterval(quizStart); 
+    generateQuestion(); 
+   }
+
+
+function generateQuestion() {
+    
+    var timeEl = document.querySelector('#time');
+    var count = 0; 
+    document.body.onload = addElement; 
+    
+
+    function addElement() {
+        const newDiv = document.createElement("div"); 
+        const newContent = document.createTextNode("TIME"); 
+      
+
+        newDiv.appendChild(newContent); 
+
+
+
+    } 
+
+    const currentDiv = document.getElementById("time");
+    document.body.insertBefore(newDiv, currentDiv); 
+
+   // incrementEl.addEventListener('click', generateQuestion);
+     //   count++;
+       // setCounterText();
+
+}; 
