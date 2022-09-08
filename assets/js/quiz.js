e3d10b223254866f5161a5cf8eac59bf0eaa13ea
Emily Hill <emhillmed2021@gmail.com>
2022-09-08 15:01:05 -0500

    display questions and answers

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -8,149 +8,173 @@
 
 
 var questionContentEl = document.getElementById("question-content")
+
 var myScore = document.querySelector(".time-head-quiz");
-
 var secondsRemain = 15;
-function setTime() {
-    var timerInterval = setInterval(function () {
-        myScore.textContent = "Time: " + secondsRemain;
 
 
-        if (secondsRemain === 0) {
-            clearInterval(timerInterval);
-            myScore.textContent = "Your score is: " + secondsRemain;
-        }
-    }
-    )
-}
 
 // questions
 
 var question = document.querySelector("question-number");
-var options = document.getElementById("button");
-
-
-var questionsList = Math.floor(Math.random() * questionBank.length)
-var currentQuestion = questionBank[questionsList]
-question.innterText = currentQuestion.question
-
+var option1 = document.getElementById("button1");
+var option2 = document.getElementById("button2");
+var option3 = document.getElementById("button3");
 
 
 
 var questionBank = [
-  
+
     {
         question: "What does the acronym API mean?",
-        options: ["1. Absolute Panic Internally","2. Apple Pie Increment","3. Application Programming Interface"],
+        option1: "1. Absolute Panic Internally", 
+        option2: "2. Apple Pie Increment", 
+        option3: "3. Application Programming Interface",
         answer: "3. Application Programming Interface"
     },
 
     {
-        question: "Why is this happening to me?",
-        options: ["1. Absolute Panic Internally","2. Apple Pie Increment","3. Application Programming Interface"],
-        answer: "3. Application Programming Interface"
+        question: "How do you comment out on JavaScript?",
+        option1: "1. *", 
+        option2: "2. //", 
+        option3: "3. #",
+        answer: "2. //"
     },
 
     {
-        question: "What is love?",
-        options: ["1. Absolute Panic Internally","2. Apple Pie Increment","3. Application Programming Interface"],
-        answer: "3. Application Programming Interface"
+        question: "How do you indicate a language in HTML?",
+        option1: "1. eng", 
+        option2: "2. lang=eng", 
+        option3: "3. lang='eng'",
+        answer: "3. lang='eng'"
     },
 
     {
         question: "Why 4?",
-        options: ["1. Absolute Panic Internally","2. Apple Pie Increment","3. Application Programming Interface"],
+        option1: "1. Absolute Panic Internally", 
+        option2: "2. Apple Pie Increment", 
+        option3: "3. Application Programming Interface", 
         answer: "3. Application Programming Interface"
     },
 
 
     {
         question: "Why 5?",
-        options: ["1. Absolute Panic Internally","2. Apple Pie Increment","3. Application Programming Interface"],
+        option1: "1. Absolute Panic Internally", 
+        option2: "2. Apple Pie Increment", 
+        option3: "3. Application Programming Interface",
         answer: "3. Application Programming Interface"
     },
 
 ]
 
+var questionsList = Math.floor(Math.random() * questionBank.length)
+var currentQuestion = questionBank[questionsList]
+// question.innerText = currentQuestion.question
+
 
 
 function quizQuestion() {
-   var questionText = document.querySelector("#questions");
+    var questionText = document.querySelector("#questions");
 
-    questionText.textContent = currentQuestion.question; 
+    questionText.textContent = currentQuestion.question;
 
-    for(i=0; i < currentQuestion.choices.length; i++) {
-        var choice = currentQuestion.options[i]//use this to dynamically create buttons or text content
-        var optionEl = document.createElement("option")
-        optionEl.className = "data-number";
-        optionEl.setAttribute("option", choice)
-        optionEl.appendChild(optionEl)
-    }
-
-    function addTextContent {
-        var element = document.createElement("text");
-        element.setAttribute("text", options)
-    }
-
-    var getButtonText = addTextContent
-    // iterate through choices 
-    //for each button, dynamically create text content 
- 
-}
-var optionsButtonHandler = function(event) {
-    var target = event.target; 
-
-    if (target.matches(".btn")) {
-        quizGame() 
-    }
-
-}
-
-
-function quizGame() {
-
-
-    if (option1 === true) {
-        window.alert("Correct!")
-        secondsRemain = secondsRemain + 5;
-    }
-    else if (option1 === false) {
-        window.alert("Incorrect!")
-        secondsRemain = secondsRemain - 5;
+    for (i = 0; i < currentQuestion.question.length; i++) {
+        var questionText = currentQuestion.question[i]
+        //use this to dynamically create buttons or text content
 
     }
 
-    if (option2 === true) {
-        window.alert("Correct!")
-        secondsRemain = secondsRemain + 5;
-    }
-    else if (option2 === false) {
-        window.alert("Incorrect!")
-        secondsRemain = secondsRemain - 5;
-    }
+    document.addEventListener("DOMContentLoaded", function() {
+    var optionOneEl = document.createElement("button");
+    optionOneEl.className = "btn"; 
+    optionOneEl.textContent= currentQuestion.option1; 
 
-    if (option3 === true) {
-        window.alert("Correct!")
-        secondsRemain = secondsRemain + 5; 
-    }
-    else if (option3 === false) {
-        window.alert("incorrect!")
-        secondsRemain = secondsRemain - 5;
-    }
+   var container = document.getElementById("container"); 
+    container.appendChild(optionOneEl);
 
+    var optionTwoEl = document.createElement("button");
+    optionTwoEl.className = "btn"; 
+    optionTwoEl.textContent = currentQuestion.option2; 
 
+   var container = document.getElementById("container"); 
+    container.appendChild(optionTwoEl);
 
+    var optionThreeEl = document.createElement("button");
+    optionThreeEl.className = "btn"; 
+    optionThreeEl.textContent = currentQuestion.option3
 
+   var container = document.getElementById("container"); 
+    container.appendChild(optionThreeEl);
 
-   
+    }, false); 
 
-}
-
-
+} 
 
 
 
-quizQuestion(); 
+// iterate through choices 
+//for each button, dynamically create text content 
 
 
-questionContentEl.addEventListener("click", optionsButtonHandler);
+
+
+
+// var optionsButtonHandler = function (event) {
+//     var target = event.target;
+
+//     if (target.matches(".btn")) {
+//         quizQuestion()
+//     }
+
+// }
+
+
+// function quizGame() {
+
+
+//     if (option1 === true) {
+//         window.alert("Correct!")
+//         secondsRemain = secondsRemain + 5;
+//     }
+//     else if (option1 === false) {
+//         window.alert("Incorrect!")
+//         secondsRemain = secondsRemain - 5;
+
+//     }
+
+//     if (option2 === true) {
+//         window.alert("Correct!")
+//         secondsRemain = secondsRemain + 5;
+//     }
+//     else if (option2 === false) {
+//         window.alert("Incorrect!")
+//         secondsRemain = secondsRemain - 5;
+//     }
+
+//     if (option3 === true) {
+//         window.alert("Correct!")
+//         secondsRemain = secondsRemain + 5;
+//     }
+//     else if (option3 === false) {
+//         window.alert("incorrect!")
+//         secondsRemain = secondsRemain - 5;
+//     }
+
+
+
+
+
+
+
+// }
+
+
+
+
+
+quizQuestion();
+
+
+
+// questionContentEl.addEventListener("click", optionsButtonHandler) 
